cb079578a254d3ec51891cea75c3fcb1e2539a56
mdulgeroglu <msd124@gmail.com>
2021-02-09 23:44:57 -0500

    Factorial calculator

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+//Write a method that calculates the factorial of a given number.
+//Factorial is the product of all positive integers less than or equal to n. For example, factorial(4) = 4x3x2x1 = 24.
+
+public class Factorial {
+
+public static void main(String args[]) {
+   System.out.println(Factorial(10));
+}
+
+static int Factorial(int n) {
+   int sum = 1;
+   for (int i=1; i<=n; i++) {
+      sum *= i;
+   }
+   return sum;
+}
+
+}
