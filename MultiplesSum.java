de3c33a149cd8004a11e327b775863c8ed5ae222
mdulgeroglu <msd124@gmail.com>
2021-02-09 19:51:38 -0500

    Initial commit

---
+++ MultiplesSum.java
@@ -0,0 +1,20 @@
+//Given a number n, write a method that sums all multiples of three and five up to n (inclusive). 
+
+public class MultiplesSum {
+
+public static void main(String args[]) {
+   int number = 55;
+   System.out.println(MultSumCalc(number));
+}
+
+static int MultSumCalc(int n) {
+   int sum = 0;
+   for (int i=0; i<=n; i++) {
+     	if (i % 3 == 0 || i % 5 == 0) {
+         sum += i;
+       }
+   }
+   return sum;
+}
+
+}
