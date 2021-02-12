8f696bbf12d8623b0a87ba774e650b7369a7df0a
mdulgeroglu <msd124@gmail.com>
2021-02-12 18:46:49 -0500

    Update lab3.sql

--- SQL/lab3.sql
+++ SQL/lab3.sql
@@ -1,7 +1,6 @@
 /* 13.
 Write a PL/SQL block to find the total monthly salary paid by the company for a given department number from the employees table. Display a message indicating whether the total salary is greater than or less than $19,000. Test your block twice using the Administration department (department_id =10) and the IT department (department_id =60). The IT department should be greater than $19,000, while the Administration department’s total should be less than $19,000.
 */
-
 DECLARE
     v_dept          employees.department_id%TYPE    := 10;
     v_limit         employees.salary%TYPE           := 19000;
@@ -27,7 +26,6 @@ BEGIN
 END;
 
 
-
 /* 16.
 Use a CASE statement:
     A.Write a PL/SQL block to select the number of countries using a supplied currency name. 
@@ -40,7 +38,6 @@ Use a CASE statement:
         display “Fewer than 10 countries”. 
 Use a CASE statement.
 */
-
 DECLARE
     v_currency          VARCHAR2(30)    := 'US DOLLAR';
     v_number_countries  PLS_INTEGER;
@@ -67,14 +64,53 @@ BEGIN
 END;
 
 
+/* 20. Write a PL/SQL block to 
+    display the country_id and country_name values 
+    from the WF_COUNTRIES table 
+    for country_id whose values range from 1 through 3. 
+Use a basic loop. Increment a variable from 1 through 3. 
+Use an IF statement to test your variable and 
+EXIT the loop after you have displayed the first 3 countries.
+COUNTRY_ID	REGION_ID	COUNTRY_NAME
+*/
+DECLARE
+    v_counter       PLS_INTEGER := 1;               -- counter
+    v_id            countries.country_id%TYPE;      -- country_id from countries table goes in here
+    v_name          countries.country_name%TYPE;    -- country_name from countries table goes in here
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(RPAD('Country ID', 12) || RPAD('Country Name', 14));
+    LOOP
+        SELECT country_id, country_name
+        INTO v_id, v_name
+        FROM countries
+        WHERE country_id = v_counter;
+        
+        DBMS_OUTPUT.PUT_LINE(RPAD(v_id, 12) || RPAD(v_name, 30));   -- output
+        
+        v_counter := v_counter + 1;         -- increment
+        
+        EXIT WHEN v_counter > 3;            -- exit
+    END LOOP;
+END;
 
 
-
-
-
-
-
-
+/* 22.Write a PL/SQL block to produce a 
+    list of available vehicle license plate numbers. 
+    These numbers must be in the following format: NN-MMM, 
+    where NN is between 60 and 65, and MMM is between 100 and 110. 
+    
+    Use nested FOR loops. 
+    The outer loop should choose numbers between 60 and 65. 
+    The inner loop should choose numbers between 100 and 110, and concatenate the two numbers together.
+*/
+DECLARE
+BEGIN
+    FOR v_count_out IN 60..65 LOOP
+        FOR v_count_in IN 100..110 LOOP
+            DBMS_OUTPUT.PUT_LINE(v_count_out || '-' || v_count_in);
+        END LOOP;
+    END LOOP;
+END;
 
 
 
