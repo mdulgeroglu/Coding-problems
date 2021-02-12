ebb5d1d7c172659fa648fc1a1cf3ff9aa9d61001
mdulgeroglu <msd124@gmail.com>
2021-02-11 23:17:39 -0500

    Create asd.sql

---
+++ SQL/asd.sql
@@ -0,0 +1,12 @@
+The following code is supposed to display the lowest and highest elevations for a country name entered by the user. However, the code does not work. Fix the code by following the guidelines for retrieving data that you learned in this lesson.
+
+DECLARE
+    v_country_name wf_countries.country_name%TYPE
+    := ‘United States of America’;
+    v_lowest_elevation wf_countries.lowest_elevation%TYPE;
+    v_highest_elevation wf_countries.highest_elevation%TYPE;
+BEGIN
+    SELECT lowest_elevation, highest_elevation
+    FROM wf_countries;
+    DBMS_OUTPUT.PUT_LINE('The lowest elevation in'||country_name||' is '||v_lowest_elevation ||’ and the highest elevation is '||v_highest_elevation||'.');
+END;
