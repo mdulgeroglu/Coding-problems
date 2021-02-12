093340382eb0f315a13e40b722a4a8d8a4eba65c
mdulgeroglu <msd124@gmail.com>
2021-02-12 17:47:49 -0500

    Revert "HighestElevation"
    This reverts commit 947a079340888fba71a7ad1eefa8f914573b5b88.

--- SQL/asd.sql
+++ SQL/asd.sql
@@ -14,13 +14,11 @@ END;
 */
 
 DECLARE
-    v_country_name          countries.country_name%TYPE          := 'United States of America';
-    v_lowest_elevation      countries.lowest_elevation%TYPE;
-    v_highest_elevation     countries.highest_elevation%TYPE;
+    v_country_name wf_countries.country_name%TYPE := 'United States of America';
+    v_lowest_elevation wf_countries.lowest_elevation%TYPE;
+    v_highest_elevation wf_countries.highest_elevation%TYPE;
 BEGIN
     SELECT lowest_elevation, highest_elevation
-    INTO v_lowest_elevation, v_lowest_elevation
-    FROM countries
-    WHERE country_name = v_country_name;
-    DBMS_OUTPUT.PUT_LINE('The lowest elevation in ' || v_country_name || ' is ' || v_lowest_elevation || ' and the highest elevation is ' || v_highest_elevation);
+    FROM wf_countries;
+    DBMS_OUTPUT.PUT_LINE('The lowest elevation in'||country_name||' is '||v_lowest_elevation ||' and the highest elevation is '||v_highest_elevation);
 END; 
