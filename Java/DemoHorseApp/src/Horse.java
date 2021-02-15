3bd26a4e2b53b9fdeef509dabb93b82deced660d
mdulgeroglu <msd124@gmail.com>
2021-02-15 17:57:00 -0500

    DemoHorseApp

---
+++ Java/DemoHorseApp/src/Horse.java
@@ -0,0 +1,33 @@
+public class Horse
+{
+    // Define the Horse class here
+    private String  name;
+    private String  color;
+    private int     birthYear;
+
+    //public methods of Horse
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setColor(String c) {
+        color = c;
+    }
+
+    public void setBirthYear(int x) {
+        birthYear = x;
+    }
+
+}
---
+++ Java/DemoHorseApp/src/RaceHorse.java
@@ -0,0 +1,13 @@
+public class RaceHorse extends Horse {
+    // additional attributes for RaceHorse
+    private int birthYear;
+
+    // additional methods for RaceHorse
+    public int getRaces() {
+        return races;
+    }
+
+    public void setRaces(int numRaces) {
+        races = numRaces;
+    }
+}
