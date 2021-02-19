f79b6ba94d6c132802c351c41bac2d36aad6303c
mdulgeroglu <msd124@gmail.com>
2021-02-18 22:21:49 -0500

    Update lab5_3.sql

--- SQL/lab5_3.sql
+++ SQL/lab5_3.sql
@@ -46,7 +46,7 @@ END;
 
 /*
 4. Using the COUNTRIES table, write a cursor that returns countries with a highest_elevation greater
-than 8,000 m. For each country, display the country_name, highest_elevation, and climate. Use a
+than 8,000  m. For each country, display the country_name, highest_elevation, and climate. Use a
 cursor FOR loop, declaring the cursor using a subquery in the FOR…LOOP statement.
 */
 
@@ -58,22 +58,32 @@ BEGIN
 END;
 
 
+/*
+5. This question uses a join of the SPOKEN_LANGUAGES and COUNTRIES tables with a GROUP
+BY and HAVING clause. Write a PL/SQL block to fetch and display all the countries that have more than six spoken
+languages. For each such country, display country_name and the number of spoken languages.
+Use a cursor FOR loop, but declare the cursor explicitly in the DECLARE section. After all the
+rows have been fetched and displayed, display an extra row showing the total number of countries
+having more than six languages. (Hint: Declare a variable to hold the value of %ROWCOUNT.)
+*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+DECLARE
+    v_count         PLS_INTEGER := 0;
+    v_pad           PLS_INTEGER := 40;
+    CURSOR countries_cur IS
+        SELECT cte.country_name, COUNT(sle.language_id) AS COUNT
+        FROM countries cte JOIN spoken_languages sle
+        ON (cte.country_id = sle.country_id)
+        GROUP BY cte.country_name
+        HAVING COUNT(*) > 6;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(RPAD('Country Name', v_pad)|| ' | ' || 'Number of Spoken Languages');
+    FOR countries_rec IN countries_cur LOOP
+        DBMS_OUTPUT.PUT_LINE (RPAD(countries_rec.country_name, v_pad) || ' | ' || countries_rec.COUNT);
+        v_count := countries_cur%ROWCOUNT;
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE(RPAD('Total', v_pad)|| ' | ' || v_count);
+END;
 
 
 
