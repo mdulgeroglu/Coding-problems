e19a7333997cca1ca91b0a4562e2fe6ae8085577
mdulgeroglu <msd124@gmail.com>
2021-02-25 23:21:47 -0500

    Update

--- Java/DemoCandlesApp/src/Candle.java
+++ Java/DemoCandlesApp/src/Candle.java
@@ -1,4 +1,15 @@
 public class Candle
 {
     // Define the Candle class here
+    private String color;
+    private int height;
+    private double price;
+
+    // get methods
+    public String getColor() {
+        return color;
+    }
+
+
+    // set methods
 }
